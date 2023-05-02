72adbabf839b1eaa84aa2d03c5a64de31c6744fa
Dr John Vidler <j.vidler@lancaster.ac.uk>
2023-05-02 12:16:45 +0100

    Work in progress for route gossip

--- main.ts
+++ main.ts
@@ -7,13 +7,16 @@ namespace SimpleRadioMesh {
         ROUTE_ANNOUNCE = 1
     }
 
+    const LOCAL_ADDRESS = control.deviceSerialNumber()
     const GOSSIP_INTERVAL = 1000
 
     let localAddress = Math.floor(Math.random() * 256)
 
     interface IPath {
-        ttl: number,
-        distance: number
+        "src": number,
+        "dst": number,
+        "ttl": number,
+        "distance": number
     }
 
     let forwardTable: IPath[] = []
@@ -27,7 +30,24 @@ namespace SimpleRadioMesh {
     })
 
     radio.onReceivedBuffer((rx) => {
-        //
+        //console.log( rx.toHex() )
+        let _type = rx.getUint8(0)
+        switch (_type) {
+            case MessageType.ROUTE_ANNOUNCE:
+                let route = parseRoutePacket(rx)
+                route.distance = route.distance + 1 // Increment the distance, its at least 1 hop from us!
+
+                console.log( route )
+
+                /*forwardTable = forwardTable.filter((v) => {
+                    if (v.dst == route.dst && route.distance < v.distance) // Filter any routes further away than this
+                        return false
+                    return true
+                })*/
+                //forwardTable.push(route) // and append our new route
+
+                break
+        }
     })
 
     control.runInBackground(() => {
@@ -42,41 +62,41 @@ namespace SimpleRadioMesh {
 
     })
 
-    function routePacket(address: number, distance: number, ttl: number): Buffer {
+    function routePacket(path: IPath): Buffer {
         let buffer = Buffer.create(11)
         buffer.setUint8(0, MessageType.ROUTE_ANNOUNCE)
-        buffer.setNumber(NumberFormat.UInt32BE, 1, control.deviceSerialNumber())
-        buffer.setNumber(NumberFormat.UInt32BE, 5, address)
-        buffer.setUint8(9, distance % 255)
-        buffer.setUint8(10, ttl % 255)
+        buffer.setNumber(NumberFormat.UInt32BE, 1, path.src)
+        buffer.setNumber(NumberFormat.UInt32BE, 5, path.dst)
+        buffer.setUint8(9, path.distance % 255)
+        buffer.setUint8(10, path.ttl % 255)
         return buffer
     }
 
-    function parseRoutePacket(buffer: Buffer): { "from": number, "to": number, "ttl": number } {
+    function parseRoutePacket(buffer: Buffer): IPath {
         return {
-            "from": buffer.getNumber(NumberFormat.UInt32BE, 1),
-            "to": buffer.getNumber(NumberFormat.UInt32BE, 5),
-            "ttl": buffer.getUint8(9)
+            "src": buffer.getNumber(NumberFormat.UInt32BE, 1),
+            "dst": buffer.getNumber(NumberFormat.UInt32BE, 5),
+            "distance": buffer.getUint8(9),
+            "ttl": buffer.getUint8(10)
         }
     }
 
     function doGossip(phase: number) {
         switch (phase) {
             case 0:
-                radio.sendBuffer(routePacket(control.deviceSerialNumber(), 0, 255))
+                radio.sendBuffer(routePacket({ src: LOCAL_ADDRESS, dst: LOCAL_ADDRESS, distance: 0, ttl: 255 }))
                 break;
 
             case 1:
                 if (forwardTable.length == 0)
                     return
 
-                let addr = Math.floor(Math.random() * forwardTable.length)
-                let row = forwardTable[addr]
-                radio.sendBuffer(routePacket(addr, row.distance, row.ttl))
+                let index = Math.floor(Math.random() * forwardTable.length)
+                radio.sendBuffer(routePacket(forwardTable[index]))
                 break;
 
             default:
-                console.log("Nothing to do in phase " + phase)
+            // Skip!
         }
     }
 }
